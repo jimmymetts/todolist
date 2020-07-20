0ada56f768df504dd4a7a67796fd60dda7f070d7
jimmymetts <jimmymetts@bellsouth.net>
2020-07-20 16:12:39 -0500

    changed duration to 200

--- src/listItems.js
+++ src/listItems.js
@@ -32,7 +32,7 @@ function ListItems(props){
         })
     return(
         <div>
-            <FlipMove duration={400} easing="ease-in-out">
+            <FlipMove duration={200} easing="ease-in-out">
             {ListItems}
             </FlipMove>
             
